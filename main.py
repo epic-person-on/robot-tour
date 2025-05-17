5cf1b69b6fda06809b3d1bf4dc4a5218cbfe63ab
Me <123142178+epic-person-on@users.noreply.github.com>
2025-05-16 18:27:55 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import sys
 sys.path.append('./lib')
 import imu
-import motordriver
+"""Uncomment this before comp !1!!111!"""
+#import motordriver
+
+if __name__ == "__main__":
+  print("Hello from the robot")
+
+
 
