08917e35dcece8cfcb558d9748b4496cb60958c9
Me <123142178+epic-person-on@users.noreply.github.com>
2025-05-19 12:27:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,17 @@ sys.path.append('./lib')
 import imu
 import motordriver
 
+# Setup Motor Driver
 m = MotorDriver()
+
+# Setup i2c interface
+i2c = machine.I2C(1, sda=machine.Pin(14), scl=machine.Pin(15))
+
+# Setup MPU6050
+mpu = MPU6050.MPU6050(i2c)
+
+# Wake up mpu
+mpu.wake()
 
 def forward(distance, time):
   pass
