813862c52c9cad5bfd1ada800a18d78e7297f404
Me <123142178+epic-person-on@users.noreply.github.com>
2025-05-30 10:07:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import sys
 import machine
 
-with open("logs.txt", "a", encoding="utf-8") as f
+with open("logs.txt", "a", encoding="utf-8") as f:
 f.write("---New Run---")
 
 # Libraries located in ./lib
